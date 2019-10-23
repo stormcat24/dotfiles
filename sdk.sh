70af0885fb92893f97032d1d8eb2462091e84c84
stormcat24 <stormcat24@stormcat.io>
2019-10-23 13:12:42 +0900

    Updated jdk

--- sdk.sh
+++ sdk.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash -e
 source ~/.sdkman/bin/sdkman-init.sh
 
-sdk install java 8.0.201-oracle
+sdk install java 8.0.222-amzn
 sdk install java 11.0.2-open 
-sdk install gradle 5.2.1
+sdk install gradle 5.6.2
 sdk install visualvm 1.4.2
-sdk install scala 2.12.8
-sdk install sbt 1.2.8
 sdk install micronaut 1.0.4 
