41f9979e22284b3597d653a070fdf1fd58129865
stormcat24 <stormcat24@stormcat.io>
2021-09-20 04:20:44 +0900

    Modify repository url of ghq

--- go.sh
+++ go.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash 
 
 # lang
-go get github.com/nsf/gocode
-go get github.com/golang/lint
-go get github.com/jstemmer/gotags
+#go get github.com/nsf/gocode
+#go get github.com/golang/lint
+#go get github.com/jstemmer/gotags
 
 # tools
-go get github.com/motemen/ghq
-go get github.com/tools/godep
-go get github.com/docker/libcontainer/nsinit
-go get github.com/rakyll/boom
-go get github.com/bazelbuild/bazelisk
+go install github.com/x-motemen/ghq@latest
+go install github.com/bazelbuild/bazelisk@latest
