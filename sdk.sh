3e6404eb7ada42fe2f7e7531fefe8354cc438d65
stormcat24 <stormcat24@stormcat.io>
2025-05-18 22:10:01 +0900

    Bump java version

--- sdk.sh
+++ sdk.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash -e
 source ~/.sdkman/bin/sdkman-init.sh
 
-sdk install java 19.0.1-zulu 
-sdk install gradle 7.6
-sdk install visualvm 2.1.5
+sdk install java 21.0.7-zulu
+sdk install gradle 8.14
+sdk install visualvm 2.1.10
