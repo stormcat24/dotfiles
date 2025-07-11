f392a0ba4d12d16b694a02fc28e22b2ff4c00538
stormcat24 <stormcat24@stormcat.io>
2025-07-11 19:20:21 +0900

    Add key repeat settings

--- hack.sh
+++ hack.sh
@@ -1,6 +1,12 @@
-#!/usr/bin/env bash -e
+#!/usr/bin/env bash
+set -e
 # OSX only
 
 # show hidden files and directories 
 defaults write com.apple.finder AppleShowAllFiles TRUE
 killall Finder
+
+# set fastest key repeat rate
+# Note: Changes take effect immediately for new apps, existing apps may need restart
+defaults write -g KeyRepeat -int 1
+defaults write -g InitialKeyRepeat -int 10
